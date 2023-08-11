876057d55dd495e4782ff06c36c51cc2b8e6de5d
markhsieh4good <79235892+markhsieh4good@users.noreply.github.com>
2023-08-11 16:44:25 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,15 @@ else
   echo "project-bridge already exist."
 fi
 
+if [ ! -e "./mysql-data" ]; then
+  echo "add folder to sync mysql data."
+  mkdir ./mysql-data
+fi
+if [ ! -e "./project" ]; then
+  echo "add folder to sync laravel project."
+  mkdir ./project
+fi
+
 if [ -z "./docker-compose.yml" ]; then
   echo "please rewrite docker-compose-example.yml to docker-compose.yml"
   exit 1
